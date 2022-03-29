0fd0269a36a7a89572908c09677e48666b9b5f0b
maggie-f <fmaggie@hotmail.com>
2022-03-29 08:12:43 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Cognizant Softvision Programmers' week 2021
+
+## Azure Service Bus tech talk
+
+
+The 7th annual Cognizant Softvision Programmers’ Week was a six day event for developers and software engineers from around the world. The 2021 event featured internal and external discussions and courses organized by one technology per day, centered on professional development, training, industry knowledge and various trending technologies, giving attendees invaluable knowledge and ideas for future innovation.
+
+### What's about?
+
+In this tech talk I talk about the functionality of **Azure Service Bus**, the patterns and protocols that it implements, its uses and when it is recommended to use it in other things.
+
+
+Here the complete talk: [Service Bus, a ride through Azure](https://www.youtube.com/watch?v=wL82DVvfNe8&list=PLh_ocureyz2fjIkZu0mH13MLKAOvtG_58&index=8).
+
+
+###### The code was based on the Microsoft's samples
+
+- [Sending and Receiving Messages](https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/servicebus/Azure.Messaging.ServiceBus/samples/Sample01_HelloWorld.md).
+
+
+###### For a deeper understanding of the subject please read the Microsoft's documentation
+[What is Azure Service Bus?](https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-messaging-overview).
+
+
---
+++ Sender/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace Sender
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://programmersweek.servicebus.windows.net/;SharedAccessKeyName=sender;SharedAccessKey=0JOMqRKwparCu9wvwz1RRnNG7Z0lyPSvokMqMYOo9Gk=;";
+        const string QueueName = "gobeyond";
+        static IQueueClient queueClient;
+        
+        public static async Task Main(string[] args)
+        {
+            var messagesToSend = GetMessagesToSend();
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send messages.
+            await SendMessagesAsync(messagesToSend);
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(string[] messagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < messagesToSend.Length; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = messagesToSend[i];
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+
+        private static string[] GetMessagesToSend()
+        {
+            string[] messages = new string[]
+            {
+                "Programmers' week 2021",
+                "Go beyond",
+                "Cognizant Softvision",
+                "Azure",
+                "Service Bus",
+            };
+
+            return messages;
+        }
+    }
+}
